29e7d5348cc8c0dd2a6c20c2ca0473fb50f683e1
Степан Зубков <zubkovbackend@gmail.com>
2023-05-31 23:10:15 +0300

    feat: Add _get_snlm0e function to Bard sync class.

--- zaebal/bard.py
+++ zaebal/bard.py
@@ -3,8 +3,11 @@
 """
 
 from typing import Dict, Optional
+import re
 
 import httpx
+
+from zaebal import config
 
 
 class Bard:
@@ -30,7 +33,12 @@ class Bard:
         This param will be used for translating from lang to English.
         :param Optional[httpx.Client] client: Httpx client. If not passed, creates new client.
         Default to None.
+        :raises: ValueError if token is invalid.
         """
+        if not token or token[-1] != ".":
+            raise ValueError(
+                "Token value must end with a single dot!"
+            )
         self.token = token
         self.timeout = timeout
         self.proxies = proxies
@@ -44,3 +52,16 @@ class Bard:
         else:
             self.client = client
         self.lang = lang
+
+
+    def _get_snlm0e(self) -> str:
+        response = self.client.get(
+            config.BARD_API_HOST, timeout=self.timeout, proxies=self.proxies,
+        )
+        response.raise_for_status()
+        SNlM0e = re.search(r"SNlM0e\":\"(.*)\"")
+        if not SNlM0e:
+            raise ValueError(
+                "SNlM0e value not found in response! Check token value."
+            )
+        return SNlM0e.group(1)
