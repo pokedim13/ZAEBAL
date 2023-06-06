906e6ed387b18a878aa23d8438aef4853576bcbf
Степан Зубков <stepanzubkov@florgon.com>
2023-06-06 19:53:55 +0300

    fix: Replace response.content by response.text

--- zaebal/bard.py
+++ zaebal/bard.py
@@ -101,7 +101,7 @@ class Bard(BaseBard):
             follow_redirects=True,
         )
         response.raise_for_status()
-        if SNlM0e := re.search(r"SNlM0e\":\"(.*)\"", response.content):
+        if SNlM0e := re.search(r"SNlM0e\":\"(.*)\"", response.text):
             return SNlM0e[1]
         else:
             raise ValueError("SNlM0e value not found in response! Check token value.")
